9726d1411bb2035c599183e11af66917a1b5fdbd
radudioane99 <52888201+radudioane99@users.noreply.github.com>
2020-03-18 18:50:52 +0200

    Laboratory 2 full

--- dioane.radu.lab2.ex01/src/dioane/radu/lab2/ex07/Ex7.java
+++ dioane.radu.lab2.ex01/src/dioane/radu/lab2/ex07/Ex7.java
@@ -9,7 +9,6 @@ public class Ex7 {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
         int x=rand.nextInt(100);
-        System.out.println(x);
         int i=0;
         while(i!=3) {
             System.out.println("Guesss a number from 0 -> 100" + " Chance number "+(i+1)+":");
